d4537f9ce2c6713ade92aa2c4d61f33cd7a5af7b
Grimm <repisk@gmail.com>
2021-09-19 19:48:56 +0500

    дизайн/адаптив карточек

--- components/ui/cards/base/card-base.component.tsx
+++ components/ui/cards/base/card-base.component.tsx
@@ -3,33 +3,36 @@ import Image from '../../image/image.component';
 import style from './card-base.module.scss';
 
 type TypeCard = {
-  id: string;
-  name: string;
-  images: any;
+	id: string;
+	name: string;
+	images: any;
 };
 
 interface CardProps {
-  item: TypeCard;
+	item: TypeCard;
 }
 
 const Card = ({ item }: CardProps): JSX.Element => {
-  const { id, name, images } = item;
+	const { id, name, images } = item;
 
-  const onClickCard = () => {
-    router.push(`/places/${id}`);
-  };
-  return (
-    <div className={style.card}>
-      <div className={style.content}>
-        {/* <div className={style.img}>
-          <Image
-            data={images[0].url}
-            alt='test'
-          />
-        </div> */}
-      </div>
-    </div>
-  );
+	const onClickCard = () => {
+		router.push(`/places/${id}`);
+	};
+	return (
+		<div className={style.card}>
+			<div className={style.content}>
+				<div className={style.img}>
+					<img src='https://via.placeholder.com/150x150' alt='123' />
+				</div>
+
+				<div className={style.text}>
+					<span>
+						text text textext text textext text textext text textext
+					</span>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Card;
