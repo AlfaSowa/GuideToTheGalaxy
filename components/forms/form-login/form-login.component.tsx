00d9437b1a47d1b45ac7b6b98c97424abc1f78c1
Sowa <Grimmino@gmail.com>
2021-10-03 20:39:53 +0500

    дизайн компонента кнопки

--- components/forms/form-login/form-login.component.tsx
+++ components/forms/form-login/form-login.component.tsx
@@ -1,4 +1,8 @@
-import { Form, Formik, validateYupSchema } from 'formik';
+import {
+  Form,
+  Formik,
+  validateYupSchema,
+} from 'formik';
 import { useRouter } from 'next/router';
 import { useState } from 'react';
 import { getToken } from '../../../methods/account';
@@ -7,72 +11,75 @@ import { Axios } from '../../../utils/axios';
 import Button from '../../ui/button/button.component';
 import TextField from '../../ui/form-fields/text-field';
 import {
-	FormContent,
-	FormActions,
-	FormItem,
-	FormTitle,
+  FormContent,
+  FormActions,
+  FormItem,
+  FormTitle,
 } from '../../ui/form/form.component';
 import style from './form-login.module.scss';
 
 const FormLogin = (): JSX.Element => {
-	const router = useRouter();
+  const router = useRouter();
 
-	const onSubmit = async ({ username, password }) => {
-		const data = await getToken({ username, password });
-		console.log(data);
-	};
+  const onSubmit = async ({ username, password }) => {
+    const data = await getToken({
+      username,
+      password,
+    });
+    console.log(data);
+  };
 
-	return (
-		<div className={style.form}>
-			<Formik
-				initialValues={{
-					username: '',
-					password: '',
-				}}
-				// validationSchema={schemaLogin}
-				onSubmit={(values) => {
-					onSubmit(values);
-				}}
-				validateOnChange={false}
-				validateOnBlur={false}
-			>
-				{({ values, handleChange }) => (
-					<Form noValidate>
-						<FormTitle>Вход</FormTitle>
+  return (
+    <div className={style.form}>
+      <Formik
+        initialValues={{
+          username: '',
+          password: '',
+        }}
+        // validationSchema={schemaLogin}
+        onSubmit={(values) => {
+          onSubmit(values);
+        }}
+        validateOnChange={false}
+        validateOnBlur={false}
+      >
+        {({ values, handleChange }) => (
+          <Form noValidate>
+            <FormTitle>Вход</FormTitle>
 
-						<FormContent>
-							<FormItem>
-								<TextField
-									placeholder='Имя:'
-									type='text'
-									name='username'
-									value={values.username}
-									onChange={handleChange}
-								/>
-							</FormItem>
+            <FormContent>
+              <FormItem>
+                <TextField
+                  placeholder='Имя:'
+                  type='text'
+                  name='username'
+                  value={values.username}
+                  onChange={handleChange}
+                />
+              </FormItem>
 
-							<FormItem>
-								<TextField
-									placeholder='Пароль:'
-									type='password'
-									name='password'
-									value={values.password}
-									onChange={handleChange}
-								/>
-							</FormItem>
-						</FormContent>
+              <FormItem>
+                <TextField
+                  placeholder='Пароль:'
+                  type='password'
+                  name='password'
+                  value={values.password}
+                  onChange={handleChange}
+                />
+              </FormItem>
+            </FormContent>
 
-						<FormActions>
-							<Button type='submit'>Войти</Button>
-							<Button onClick={() => router.push('/')}>
-								Отмена
-							</Button>
-						</FormActions>
-					</Form>
-				)}
-			</Formik>
-		</div>
-	);
+            <FormActions>
+              <Button type='submit'>Войти</Button>
+              <Button onClick={() => router.push('/')}>
+                Отмена
+              </Button>
+            </FormActions>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
 };
 
 export default FormLogin;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,29 +1,32 @@
 import '../styles/globals.scss';
 import { AppProps } from 'next/app';
-import { $pages, getPagesDataFx } from '../models/pages';
 import { useStore } from 'effector-react';
 import { useEffect } from 'react';
+import {
+  $pages,
+  getPagesDataFx,
+} from '../models/pages';
 import { getAccountFx } from '../models/account';
 
 const WrappedApp = ({ Component, pageProps }: AppProps): JSX.Element => {
-	const pages = useStore($pages);
+  const pages = useStore($pages);
 
-	useEffect(() => {
-		if (!pages.length) {
-			getPagesDataFx();
-		}
-	}, []);
+  useEffect(() => {
+    if (!pages.length) {
+      getPagesDataFx();
+    }
+  }, []);
 
-	useEffect(() => {
-		const reg = /(?:(?:^|.*;\s*)token\s*\=\s*([^;]*).*$)|^.*$/;
-		const token = document.cookie ? document.cookie.replace(reg, '$1') : '';
+  useEffect(() => {
+    const reg = /(?:(?:^|.*;\s*)token\s*\=\s*([^;]*).*$)|^.*$/;
+    const token = document.cookie ? document.cookie.replace(reg, '$1') : '';
 
-		if (token) {
-			getAccountFx(token);
-		}
-	}, []);
+    if (token) {
+      getAccountFx(token);
+    }
+  }, []);
 
-	return <Component {...pageProps} />;
+  return <Component {...pageProps} />;
 };
 
 export default WrappedApp;
