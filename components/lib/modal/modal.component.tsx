7fe17659cb08aef352c039d8cede0c9884a92616
Grimm <repisk@gmail.com>
2021-10-03 22:21:50 +0500

    логика модалки

--- components/lib/modal/modal.component.tsx
+++ components/lib/modal/modal.component.tsx
@@ -1,85 +1,82 @@
 import { createPortal } from 'react-dom';
-import {
-  ReactNode,
-  useEffect,
-} from 'react';
+import { ReactNode, useEffect } from 'react';
 import CloseIcon from '@mui/icons-material/Close';
 import clsx from 'clsx';
 import styles from './modal.module.scss';
 
 interface ModalProps {
-  textHeader: string;
-  children: ReactNode;
-  maxWidth: number;
-  open: boolean;
-  onClose: any;
+	textHeader: string;
+	children: ReactNode;
+	open: boolean;
+	onClose: any;
 }
 
 const Modal = ({
-  textHeader,
-  children,
-  maxWidth,
-  open,
-  onClose,
+	textHeader,
+	children,
+	open,
+	onClose,
 }: ModalProps): JSX.Element => {
-  useEffect(() => {
-    const { userAgent } = window.navigator;
+	const onOverlayClick = (e) => {
+		if (e.target.className === styles.modal) {
+			onClose(false);
+		}
+	};
 
-    const reqMacintosh = /Macintosh/i;
+	useEffect(() => {
+		const { userAgent } = window.navigator;
 
-    if (open) {
-      if (!reqMacintosh.exec(userAgent)) {
-        document.body.classList.add('noMac');
-      }
+		const reqMacintosh = /Macintosh/i;
 
-      document.body.classList.add('hidden');
-    } else {
-      document.body.classList.remove('hidden');
-      if (document.body.classList.contains('noMac')) {
-        document.body.classList.remove('noMac');
-      }
-    }
+		if (open) {
+			if (!reqMacintosh.exec(userAgent)) {
+				document.body.classList.add('noMac');
+			}
 
-    return () => {
-      document.body.classList.remove('hidden');
-      if (document.body.classList.contains('noMac')) {
-        document.body.classList.remove('noMac');
-      }
-    };
-  }, [open]);
+			document.body.classList.add('hidden');
+		} else {
+			document.body.classList.remove('hidden');
+			if (document.body.classList.contains('noMac')) {
+				document.body.classList.remove('noMac');
+			}
+		}
 
-  return (
-    <>
-      {open
-				&& createPortal(
-				  <div className={styles.modal}>
-				    <div
-				      className={styles.modal__inner}
-				      style={{ maxWidth: `${maxWidth}px` }}
-  >
-				      <div className={styles.modal__header}>
-				        <span>{textHeader}</span>
+		return () => {
+			document.body.classList.remove('hidden');
+			if (document.body.classList.contains('noMac')) {
+				document.body.classList.remove('noMac');
+			}
+		};
+	}, [open]);
 
-				        <button
-				          className={styles.btn}
-				          onClick={() => onClose(false)}
-  >
-				          <CloseIcon />
-  </button>
-  </div>
+	return (
+		<>
+			{open &&
+				createPortal(
+					<div className={styles.modal} onClick={onOverlayClick}>
+						<div className={styles.modal__inner}>
+							<div className={styles.modal__header}>
+								<span>{textHeader}</span>
 
-				      <div>{children}</div>
-  </div>
-  </div>,
-				  document.body,
+								<button
+									className={styles.btn}
+									onClick={() => onClose(false)}
+								>
+									<CloseIcon />
+								</button>
+							</div>
+
+							<div>{children}</div>
+						</div>
+					</div>,
+					document.body
 				)}
-    </>
-  );
+		</>
+	);
 };
 
 Modal.defaultProps = {
-  textHeader: 'Текст модального окна',
-  maxWidth: 500,
+	textHeader: null,
 };
 
 export default Modal;
