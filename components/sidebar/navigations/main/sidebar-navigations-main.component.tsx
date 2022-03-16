19e8947158dfc219584f7d0aee160e6e43fe4578
Sowa <Grimmino@gmail.com>
2022-03-16 21:16:37 +0500

    фикс конфликтов

--- components/sidebar/navigations/main/sidebar-navigations-main.component.tsx
+++ components/sidebar/navigations/main/sidebar-navigations-main.component.tsx
@@ -1,24 +1,18 @@
-import { useStore } from "effector-react";
 import { FC } from "react";
-import { $account } from "../../../../models/account";
 import AccordionItem from "../../../ui/accordion/accordion-item.component";
 import Accordion from "../../../ui/accordion/accordion.component";
 import SidebarMenuItem from "../../components/menu/sidebar-menu-item.component";
 import SidebarMenu from "../../components/menu/sidebar-menu.component";
 
-const SidebarNavigationsMain: FC = () => {
-  const account = useStore($account);
-
-  return (
-    <SidebarMenu>
-      <SidebarMenuItem>
-        <Accordion>
-          <AccordionItem>1</AccordionItem>
-          <AccordionItem>2</AccordionItem>
-        </Accordion>
-      </SidebarMenuItem>
-    </SidebarMenu>
-  );
-};
+const SidebarNavigationsMain: FC = () => (
+  <SidebarMenu>
+    <SidebarMenuItem>
+      <Accordion>
+        <AccordionItem>1</AccordionItem>
+        <AccordionItem>2</AccordionItem>
+      </Accordion>
+    </SidebarMenuItem>
+  </SidebarMenu>
+);
 
 export default SidebarNavigationsMain;
