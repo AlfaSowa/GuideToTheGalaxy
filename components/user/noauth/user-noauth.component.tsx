e56233871518cacd9d7c3aef51b67aba278ee10f
Grimm Fontano <repisk@gmail.com>
2022-03-24 18:31:05 +0500

    добавил роутеры на страницы регистрации/логина соискателя

--- components/user/noauth/user-noauth.component.tsx
+++ components/user/noauth/user-noauth.component.tsx
@@ -1,31 +1,20 @@
-import { FC, useCallback, useState } from "react";
+import { FC, useCallback } from "react";
 import { useRouter } from "next/router";
 import Button from "../../ui/button/button.component";
 import styles from "./styles.module.scss";
-import Modal from "../../lib/modal/modal.component";
-import {
-  CustomTabs,
-  Tab,
-  Tabs,
-  TabsPanel,
-} from "../../lib/tabs/tabs.component";
 import { useAccount } from "../../../hooks/account/useAccount";
-import ApplicantLoginForm from "../../forms/applicant/login/applicant-login.component";
-import ApplicantRegistrationForm from "../../forms/applicant/registration/applicant-registration.component";
-
-enum FormType {
-  LOGIN = "login",
-  REGISTR = "registration",
-}
 
 const UserNoAuth: FC = () => {
   const { fetchingAccount } = useAccount();
-  const [openModal, setOpenModal] = useState<{ value: FormType }>(null);
 
   const router = useRouter();
 
   const handleClickRegistration = useCallback(() => {
     router.push("/auth/applicant");
+  }, [router]);
+
+  const handleClickLogin = useCallback(() => {
+    router.push("/account/applicant");
   }, [router]);
 
   if (fetchingAccount) {
@@ -37,41 +26,13 @@ const UserNoAuth: FC = () => {
   }
 
   return (
-    <>
-      <div className={styles.root}>
-        <Button onClick={() => setOpenModal({ value: FormType.LOGIN })}>
-          Вход
-        </Button>
+    <div className={styles.root}>
+      <Button onClick={handleClickLogin}>Вход</Button>
 
-        <Button variant="secondary" onClick={handleClickRegistration}>
-          Регистрация
-        </Button>
-      </div>
-
-      <Modal
-        textHeader="Вход"
-        open={!!openModal}
-        onClose={() => setOpenModal(null)}
-      >
-        <CustomTabs>
-          <Tabs
-            value={openModal?.value}
-            onChange={(value: FormType) => setOpenModal({ value })}
-          >
-            <Tab value="login">Войти</Tab>
-            <Tab value="registration">Зарегистрироваться</Tab>
-          </Tabs>
-
-          <TabsPanel value={openModal?.value} index="login">
-            <ApplicantLoginForm />
-          </TabsPanel>
-
-          <TabsPanel value={openModal?.value} index="registration">
-            <ApplicantRegistrationForm />
-          </TabsPanel>
-        </CustomTabs>
-      </Modal>
-    </>
+      <Button variant="secondary" onClick={handleClickRegistration}>
+        Регистрация
+      </Button>
+    </div>
   );
 };
 export default UserNoAuth;
